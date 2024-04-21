48c9e2759ca45c5873e855f68bc244acd4705f27
Lorenzo282001 <lorenzorubino28@gmail.com>
2024-04-21 09:20:02 +0200

    Added server node for backend to TwitchBank_db [localhost]

---
+++ backend/main.js
@@ -0,0 +1,31 @@
+// Server Node per entrare nel database
+const nodemon = require('nodemon');
+
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Import the cors middleware
+
+const app = express();
+
+
+// Middleware to parse JSON and form data
+app.use(cors()); // Use the cors middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MySQL connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'twitchbank_db',
+});
+
+connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+});
