ff11226aec251ef80831546f4b63bbd834d3ef34
Lorenzo282001 <lorenzorubino28@gmail.com>
2024-04-26 10:51:41 +0200

    -

---
+++ javascript/homepage/homepageBanca.js
@@ -0,0 +1,14 @@
+// QUI CARICO GLI SCRIPT CHE GESTISCONO IN HOMEPAGE
+// TUTTO QUELLO CHE RIGUARDA LA BANCA
+
+// on document ready
+$(document).ready(function () {
+    
+});
+
+// on window change size 
+$(window).resize(function() {
+
+
+
+});
